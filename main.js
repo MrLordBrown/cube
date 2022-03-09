34934cd2742afbccca172dd0506c54b5f34584ce
Christopher Brown <MrLordBrown@users.noreply.github.com>
2022-03-08 17:34:48 -0800

    init

--- main.js
+++ main.js
@@ -1,64 +1,158 @@
 
-const data = [
-	"Data": [{
-		"id": "A",
-		"Front": "The boat is on the lake. You",
-		"Right": "swim",
-		"Left": "fall",
-		"Bottom": "hop",
-		"Top": "turn",
-		"image": null
-	}, {
-		"id": "swim",
-		"Front": "swim towards it briskly.",
-		"Right": null,
-		"Left": null,
-		"Bottom": null,
-		"Top": null,
-		"image": null
-	}, {
-		"id": "fall",
-		"Front": "fall asleep on the pier.",
-		"Right": null,
-		"Left": null,
-		"Bottom": null,
-		"Top": null,
-		"image": null
-	}, {
-		"id": "hop",
-		"Front": "hop in the kayak and paddle for it.",
-		"Right": null,
-		"Left": null,
-		"Bottom": null,
-		"Top": null,
-		"image": null
-	}, {
-		"id": "turn",
-		"Front": "turn and dissappear into the cabin.",
-		"Right": null,
-		"Left": null,
-		"Bottom": null,
-		"Top": null,
-		"image": null
-	}]
+
+const data =[
+  {
+    "id": "A",
+    "front": "The boat is on the lake. You",
+    "right": "swim",
+    "left": "fall",
+    "bottom": "hop",
+    "top": "turn",
+    "image": null
+  },
+  {
+    "id": "swim",
+    "front": "swim towards it briskly.",
+    "right": "jug",
+    "left": "sump",
+    "bottom": "run",
+    "top": "oil",
+    "image": null
+  },
+  {
+    "id": "fall",
+    "front": "fall asleep on the pier.",
+    "right": null,
+    "left": null,
+    "bottom": null,
+    "top": null,
+    "image": null
+  },
+  {
+    "id": "hop",
+    "front": "hop in the kayak and paddle for it.",
+    "right": null,
+    "left": null,
+    "bottom": null,
+    "top": null,
+    "image": null
+  },
+  {
+    "id": "turn",
+    "front": "turn and dissappear into the cabin.",
+    "right": null,
+    "left": null,
+    "bottom": null,
+    "top": null,
+    "image": null
+  },{
+    "id": "jug",
+    "front": "jug your mama bits.",
+    "right": null,
+    "left": null,
+    "bottom": null,
+    "top": null,
+    "image": null
+  },{
+    "id": "sump",
+    "front": "sump up the agent.",
+    "right": null,
+    "left": null,
+    "bottom": null,
+    "top": null,
+    "image": null
+  },
+  {
+    "id": "run",
+    "front": "run the swaggert.",
+    "right": null,
+    "left": null,
+    "bottom": null,
+    "top": null,
+    "image": null
+  },
+  {
+    "id": "oil",
+    "front": "oil the grizzty.",
+    "right": null,
+    "left": null,
+    "bottom": null,
+    "top": null,
+    "image": null
+  }
 ];
+
+let f = document.querySelector('#f');
+let l = document.querySelector('#l');
+let r = document.querySelector('#r');
+let t = document.querySelector('#t');
+let d = document.querySelector('#d');
+let b = document.querySelector('#b');
+let tp = document.querySelector("#top");
+let bm = document.querySelector("#bottom");
+let lt = document.querySelector("#left");
+let rt = document.querySelector("#right");
+
+f.innerHTML = data[0].front;
+tp.innerHTML = data[0].top;
+bm.innerHTML = data[0].bottom;
+lt.innerHTML = data[0].left;
+rt.innerHTML = data[0].right;
+let current;
+let go;
+let x;
+
+function doIt(x,y) {
+console.log(x);
+for (var i = 0; i < count; i++) {
+  console.log(data[i].id);
+    if (data[i].id == x) {
+f.innerHTML = data[i].front;
+console.log(f.innerHTML);
+tp.innerHTML = data[i].top;
+bm.innerHTML = data[i].bottom;
+lt.innerHTML = data[i].left;
+rt.innerHTML = data[i].right;
+}
+}
+if (y == 't') {
+  d.id = 'b';
+  f.id = 't';
+  t.id = 'f';
+} else if (y == 'd') {
+  t.id = 'b';
+  f.id = 't';
+  d.id = 'f';
+} else if (y == 'l') {
+l.id = 'b';
+  f.id = 'l';
+  r.id = 'f';
+} else if (y == 'r') {
+  r.id = 'b';
+  f.id = 'r';
+  l.id = 'f';
+}
+}
 
 var count = Object.keys(data).length;
 
 let cube = document.querySelector('#kyub');
-
-function up() {
+function upy() {
   cube.style.transform = 'rotateX(90deg)';
+  doIt(top.innerHTML,'t');
 }
 
-function down() {
+function downy() {
   cube.style.transform = 'rotateX(-90deg)';
+  doIt(bottom.innerHTML,'d');
 }
 
-function left() {
+function lefty() {
   cube.style.transform = 'rotateY(-90deg)';
+  doIt(left.innerHTML,'l');
 }
 
-function right() {
+function righty() {
   cube.style.transform = 'rotateY(90deg)';
+  doIt(right.innerHTML,'r');
 }
